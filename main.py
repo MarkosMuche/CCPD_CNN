b08b071919a55022fd63254fd9c92fb19bd52001
MarkosMuche <markosmuche2018@gmail.com>
2022-07-05 06:40:41 +0300

    colab working

--- main.py
+++ main.py
@@ -291,14 +291,14 @@ if __name__ == '__main__':
         # PIL图片上打印汉字
         pilImg = Image.fromarray(cv2.cvtColor(cv2Img, cv2.COLOR_BGR2RGB))
         draw = ImageDraw.Draw(pilImg)
-        font = ImageFont.truetype("simhei.ttf", 40, encoding="utf-8")  # 参数1：字体文件路径，参数2：字体大小
+        font = ImageFont.truetype(r'/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf', 70)
         draw.text((int(left_up[0]), int(left_up[1]) - 40), lpn, (255, 0, 0),
-                  font=font)  # 参数1：打印坐标，参数2：文本，参数3：字体颜色，参数4：字体
+                 font=font )  # 参数1：打印坐标，参数2：文本，参数3：字体颜色，参数4：字体
         # PIL图片转cv2 图片
         cv2charimg = cv2.cvtColor(np.array(pilImg), cv2.COLOR_RGB2BGR)
-        # cv2.putText(cv2Img, lpn, (int(left_up[0]), int(left_up[1])-20), cv2.FONT_ITALIC, 2, (0, 0, 255))
+        cv2.putText(cv2Img, lpn, (int(left_up[0]), int(left_up[1])-20), cv2.FONT_ITALIC, 2, (0, 0, 255))
         dstFileName = 'result/' + ims[0][-5:-4] + '.jpg'
-        cv2.imshow('charimg',cv2charimg)
-        cv2.waitKey(0)
-        # cv2.imwrite(dstFileName, cv2charimg)
-        # print('图片保存地址', dstFileName)
+        # cv2.imshow('charimg',cv2charimg)
+        # cv2.waitKey(0)
+        cv2.imwrite(dstFileName, cv2charimg)
+        print('图片保存地址', dstFileName)
